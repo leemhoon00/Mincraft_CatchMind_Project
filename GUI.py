4c75c6c8a9af163f0d64ed46ad8cd70a3dfe3952
Kong Gyeong-il <nittoo@naver.com>
2022-12-22 21:20:09 +0900

    gui 서버 통합
    gui에서 채팅시 서버로 메시지 전송

--- GUI.py
+++ GUI.py
@@ -1,24 +1,36 @@
 from tkinter import *
+import socketio
+
+sio = socketio.Client()
 
 class Example(Frame):
     def __init__(self):
         super().__init__()
-
         self.initUI()
+        sio.connect('http://ec2-54-249-31-97.ap-northeast-1.compute.amazonaws.com:3000')#서버 연결
 
     def callback(self, event):
-        print(self.name.get())
-
+        args = self.name.get().split()
+        if(args[0] == '/setAnswer'):
+            print('정답 등록완료')
+            sio.emit('setAnswer', args[1])
+        elif(args[0] == '/setPlayername'):  
+            print('player이름 등록완료')
+            self.playerName = args[1]
+        else:
+            sio.emit('answerCheck',{'player' : self.playerName,'answer':args[0]})
+        self.textbox.delete(0, END)
+        
     def initUI(self):
         self.master.title("Centered window")
         self.pack(fill=BOTH, expand=1)
         self.centerWindow()
         self.name = StringVar()
 
-        textbox = Entry(self,textvariable=self.name)
-        textbox.grid(column=0,row=0)
-        textbox.place(x=0,y=0,relwidth=0.5,relx=0.25)
-        textbox.bind('<Return>',self.callback)
+        self.textbox = Entry(self,textvariable=self.name)
+        self.textbox.grid(column=0,row=0)
+        self.textbox.place(x=0,y=0,relwidth=0.5,relx=0.25)
+        self.textbox.bind('<Return>',self.callback)
 
     def centerWindow(self):
         w = self.master.winfo_screenwidth()
@@ -31,9 +43,28 @@ class Example(Frame):
         # y = (sh - h)/6
         y=0
         self.master.geometry('%dx%d+%d+%d' % (w,h,x,y))
+        
 
+    @sio.on('answerCheck')#정답 체크하는 이벤트
+    def checkAnswer(data):
+        if(data == 1):
+            print("아쉽지만 늦었습니다.")
+        elif(data == 2):
+            print("정답이 등록되지않았습니다.")
+        else:
+            print("오답입니다.")
+
+    @sio.on('answerWinner')#우승자가 나올시 메시지 출력 이벤트
+    def winner(data):
+        print(data + "가 정답을 맞췄습니다.")
+    
+    def __del__(self):
+        sio.disconnect()#프로그램 종료시에 실행하여 서버분리
+    
 def Show_Textbox():
     root = Tk()
     ex = Example()
     root.mainloop()
+        
+
 
--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from mcpi.minecraft import Minecraft
 import Draw_Screen as ds
 from mcpi import entity
 from mcpi import event
-import GUI
+import GUI as g
 
 mc = Minecraft.create()
+
 test = ds.RGBBlock(mc)
 test.setPaint128Location() #한번만 실행,도화지 그릴위치와 주변 환경정리
 test.setPixLocation() #이미 맵이 완성되었으면 위치만 잡아주기
 
-pix = camera.capture()
+g.Show_Textbox()#채팅창 띄우기
 
+pix = camera.capture()
 test.print2DtoBlocks(pix) #128X128PIX데이터(RGB) 입력시 프린터
